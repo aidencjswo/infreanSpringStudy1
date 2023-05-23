b06eeeee4d3878416571278ffd0b9274c3da5f20
sangyeong <aidencjswo@gmail.com>
2023-05-23 21:27:10 +0900

    ~ @Configuration과 바이트코드 조작의 마법

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -1,5 +1,6 @@
 package hello.core.singleton;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,9 +16,17 @@ class StatefulServiceTest {
 
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
-
-        statefulService1.order("userA", 10000);
-        statefulService1.order("userB", 20000);
+        
+        //ThreadA : A사용자 10000원 주문
+        int userA = statefulService1.order("userA", 10000);
+        //ThreadB : B사용자 20000원 주문
+        int userB = statefulService2.order("userB", 20000);
+        
+        //ThreadA : 사용자A가 주문 금액 조회
+//        int price = statefulService1.getPrice();
+//        System.out.println("price = " + price);
+//
+//        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig{
